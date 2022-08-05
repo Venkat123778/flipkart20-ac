63aa2a6cee34b0e0c66c252ccc3cecfba038505a
Venkat <krishnametla123@gmail.com>
2022-08-05 16:31:19 +0530

    dev code for flipkart 20

---
+++ src/test/java/flipkart20 ac/HelloWorldPortTypeImplTest.java
@@ -0,0 +1,14 @@
+package flipkart20 ac;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldPortTypeImplTest {
+
+    @Test
+	public void testSayHi() {
+        HelloWorldPortTypeImpl helloWorldPortTypeImpl = new HelloWorldPortTypeImpl();
+        String response = helloWorldPortTypeImpl.sayHi("Sam");
+        assertEquals("HelloWorldPortTypeImpl not properly saying hi", "Hello Sam", response);
+    }
+}
